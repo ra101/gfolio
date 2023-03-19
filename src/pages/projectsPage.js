d79c759757865cdbc4c7f88a129a73e7b0748a2e
Parth Agarwal <agarwal.parth.101@protonmail.com>
2023-03-19 16:57:31 +0530

    github api to populate project query

--- src/pages/projectsPage.js
+++ src/pages/projectsPage.js
@@ -1,30 +1,56 @@
-import React from "react";
-import { ProjectsContent } from "../data/projectsContent";
+import React, {useState} from "react";
+import { useLocation } from "react-router-dom";
 import "./projectsPage.css";
 import FilterMenu from "../components/filtermenu";
 
-const ProjectsPage = ({ results }) => {
+
+export default function ProjectsPage() {
+
+  const startOffset = new URLSearchParams(
+    useLocation().search
+  ).get('start');
+
+  const [repoData, setRepoData] = useState([]);
+  // https://api.github.com/users/ra101/repos?per_page=1000
+  // https://opengraph.githubassets.com/0/ra101/kissSolver
+
+  // console.log(repoData);
+
+  (async () => {
+    if (!repoData.length){
+      const response = await fetch("https://api.github.com/users/ra101/repos?per_page=1000");
+      if (response.status == 200){
+        const data = await response.json();
+        setRepoData(data);
+      }
+    }
+  })();
+
+  // startOffset
+
   return (
     <div className="main">
             <FilterMenu />
       <div className="all-results-container">
         <p className="result-count">
-          About {ProjectsContent.length} results (0.84 seconds)
+          About {repoData.length} results (0.84 seconds)
         </p>
         <div className="projects-content">
-          {ProjectsContent.map((item) => (
+          {repoData.map((item) => (
             <div className="projects-card">
-              <a href={`${item.link}`} className="project-link">
-                <p> {`${item.link}`} </p>
-                <h3>{`${item.name}`}</h3>
+              <a href={`${item.html_url}`} className="project-link">
+                <p> {`${item.html_url}`} </p>
+                <h3>{`${item.name}${item.description?" - " + item.description:""}`}</h3>
               </a>
               <div className="projects-details">
                 <div className="projects-img-container">
-                  <img src={item.img} alt={item.name} />
+                  <img src={
+                    "https://opengraph.githubassets.com/0/ra101/" + item.name
+                    } alt={item.name} />
                 </div>
                 <div className="projects-text-container">
-                  <p className="projects-excerpt">{`${item.excerpt}`}</p>
-                  <p className="projects-tools">{`${item.tools}`}</p>
+                  <p className="projects-excerpt">{`${item.description}`}</p>
+                  <p className="projects-tools">{`${item.language}`}</p>
                 </div>
               </div>
             </div>
@@ -34,5 +60,3 @@ const ProjectsPage = ({ results }) => {
     </div>
   );
 };
-
-export default ProjectsPage;
